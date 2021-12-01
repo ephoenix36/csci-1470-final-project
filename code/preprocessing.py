901e15cd5639b09a93353676b809fa793273a7f9
Paul Molnar <paul_molnar@brown.edu>
2021-11-30 20:48:09 -0500

    need to commit

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import json
 from skimage import io
-from pycocotools.coco import COCO as cocoDataset
+# from pycocotools.coco import COCO as cocoDataset
 import fiftyone as fo
 import fiftyone.zoo as foz
 
@@ -12,34 +12,10 @@ def get_data(training_file, testing_file):
     gets the training data from the data folder in the json file
     """
 
-    data_ids = {}
-    # data_ids['train_ids'] = np.load("../data/coco_train_ids.npy")
-    # data_ids['test_ids'] = np.load("../data/coco_test_ids.npy")
-    # data_ids['dev_ids'] = np.load("../data/coco_dev_ids.npy")
-    # data_ids['restval_ids'] = np.load("../data/coco_restval_ids.npy")
-
-    initialized_coco = cocoDataset(annotation_file="../data/captions_train2014.json")
-
-    # print(initialized_coco.imgs)
-    # print(initialized_coco.anns)
-
-    data_ids['train_ids'] = list(initialized_coco.imgs.keys())
-    data_ids['test_ids'] = np.load("../data/coco_test_ids.npy")
-    data_ids['dev_ids'] = np.load("../data/coco_dev_ids.npy")
-    data_ids['restval_ids'] = np.load("../data/coco_restval_ids.npy")
-
-    print(data_ids['train_ids'])
-
-    a = initialized_coco.download(tarDir="/Users/jacobaxel/deepLearning/projects/csci-1470-final-project/data", imgIds=[data_ids['train_ids'][0]])
-    # print(a)
-    # one_image = initialized_coco.loadImgs([3, 6])
-    # print(one_image)
-
-
     # fiftyone stuffff
 
     #
-    # Load 50 random samples from the validation split
+    # Load 50 random samples from the training split
     #
     # Only the required images will be downloaded (if necessary).
     # By default, only detections are loaded
