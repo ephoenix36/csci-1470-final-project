ce50ef771a42de9153b6bdb26568598aede9a1d7
Enahm <enahm_oscar-phoenix@brown.edu>
2021-12-08 20:15:37 -0500

    merging

--- model/transformer/decoder.py
+++ model/transformer/decoder.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Layer, Dense, LayerNormalization, Dropout
 
-from transformer.Attention import MultiHeadAttention
+from transformer.attention import MultiHeadAttention
 from transformer.utils import *
 
 class MeshedDecoderLayer(Layer):
